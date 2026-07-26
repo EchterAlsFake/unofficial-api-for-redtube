230888f9a15fdc19ffbd779755d3dc8bd77245c9
Johannes Habel <EchterAlsFake@proton.me>
2026-07-26 20:58:57 +0200

    - switched license to AGPL - release

--- redtube_api/__init__.py
+++ redtube_api/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ["Client", "Video", "Channel", "Playlist", "Pornstar"]
+__all__ = ["Client", "Video", "Channel", "Playlist", "Pornstar", "DownloadConfigHLS"]
 
-from redtube_api.api import Video, Client, Channel, Playlist, Pornstar
+from redtube_api.api import Video, Client, Channel, Playlist, Pornstar, DownloadConfigHLS
